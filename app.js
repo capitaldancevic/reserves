628831fa0dbbfc985091f5e685641146ae0ef699
capitaldancevic <capitaldancevideos@gmail.com>
2026-02-22 11:24:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+// IMPORTS FIREBASE
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCyR9t974slq_63KePie4stTqg3fbN9uD4",
+  authDomain: "capital-dance-vic-reserves.firebaseapp.com",
+  projectId: "capital-dance-vic-reserves",
+  storageBucket: "capital-dance-vic-reserves.firebasestorage.app",
+  messagingSenderId: "550487696683",
+  appId: "1:550487696683:web:e15891d9c9dab4512cd16e"
+};
+
+// INIT
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
